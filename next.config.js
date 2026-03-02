967a6d7daeebe7060691ad3df7f009db2f6b709f
Ashwin Revankar <ashrev2000@gmail.com>
2026-03-02 14:35:08 +0530

    fix content folder on Vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  outputFileTracingIncludes: {
+    '/blog/[slug]': ['./content/blog/**/*'],
+  },
 };
 
 module.exports = nextConfig;
